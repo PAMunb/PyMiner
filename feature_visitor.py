a167f5b096ae404c87b9a31744cb8f1ae4a1e33e
waltim <waltimlmm@gmail.com>
2024-10-07 17:48:55 -0300

    removendo código desnecessário e acrescentando mais alguns testes.

--- feature_visitor.py
+++ feature_visitor.py
@@ -4,22 +4,23 @@ class FeatureVisitor(ast.NodeVisitor):
     def __init__(self, source_code):        
         self.feature_num_literals = 0
         self.source_code = source_code.splitlines()
+        self.all_stmts = 0
+
+    def generic_visit(self, node):
+        # Chama o método de visitação adequado para cada tipo de nó
+        if isinstance(node, ast.stmt):
+            # print(f'Encontrado node Stmt: {ast.dump(node, annotate_fields=True, indent=1)}')
+            self.all_stmts += 1
+        super().generic_visit(node)
+        
     
     def visit_Constant(self, node):
         # Verifica se o node é um número
+        # print(f'Encontrado Constante: {ast.dump(node, annotate_fields=True, indent=1)}')
         if isinstance(node.value, (int, float)):
             # Extrai a linha do código-fonte e verifica se há '_'
             literal_source = self.source_code[node.lineno - 1][node.col_offset:node.end_col_offset]
             if '_' in literal_source:
+                # print(f'Encontrado Underscore: {ast.dump(node, annotate_fields=True, indent=1)}')
                 self.feature_num_literals += 1
         self.generic_visit(node)
-
-    def visit_Num(self, node):
-        # Para compatibilidade com versões anteriores do Python (antes do 3.8)
-        if isinstance(node.n, (int, float)):
-            literal_source = self.source_code[node.lineno - 1][node.col_offset:node.end_col_offset]
-            if '_' in literal_source:
-                self.feature_num_literals += 1
-                
-        self.generic_visit(node)
-
--- tests/resources/num_literals.py
+++ tests/resources/num_literals.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+
+
 w = 1_000
 x = 1_000_000
 y = 123_456.789_012
@@ -16,3 +19,11 @@ class num():
         d = 1_000e10
         e = 1.23_45e2
     
+    
+num1 = 1_000_000
+num2 = 3.14159_26535
+num3 = int(1_234)  # int com underscore
+num4 = float(2_718)  # float com underscore
+num5 = complex(3_14, 1_59)  # complex com underscores
+num6 = Decimal(1_000.00)  # Decimal com underscore
+num7 = 123456  # sem underscore
